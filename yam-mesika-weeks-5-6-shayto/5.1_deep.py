ace554b4108f3a9564e4708990aaa03d73ba6f51
Shay <shaindel720@gmail.com>
2022-03-30 11:53:49 +0300

    Deep program.

--- yam-mesika-weeks-5-6-shayto/5.1_deep.py
+++ yam-mesika-weeks-5-6-shayto/5.1_deep.py
@@ -1,17 +1,19 @@
 import os
 
-DEEP = "deep"
-FILE_MSG = "insert the path of the wanted folder"
-
 
 def thisIsTheWay() -> object:
-    # function to read the path and create a list of the files that start with deep
-    # and prints them
-    folder_path = input("insert the path of the wanted folder")
-    dir_list = os.listdir(folder_path)
-    starts_with_deep = [f for f in dir_list if f.startswith(DEEP)]
+    """
+    function to read the file path and create a list of the files that start with deep
+    and prints them.
+    :return: none
+    """
+    folder_path = input("Insert the path of the wanted folder")
+    files_list = os.listdir(folder_path)
+    starts_with_deep = [file_path for file_path in files_list if file_path.startswith("deep")]
     print(starts_with_deep)
 
 
 if __name__ == '__main__':
     thisIsTheWay()
+
+
