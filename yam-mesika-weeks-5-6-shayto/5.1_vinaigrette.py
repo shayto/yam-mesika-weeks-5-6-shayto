e25adb5ff48f785cbea64aaeb6f8c74c19ef1c9c
Shay <shaindel720@gmail.com>
2022-03-30 11:59:24 +0300

    Vinaigrette program.

--- yam-mesika-weeks-5-6-shayto/5.1_vinaigrette.py
+++ yam-mesika-weeks-5-6-shayto/5.1_vinaigrette.py
@@ -2,35 +2,44 @@ import random
 import time
 import datetime
 
-INPUT_DATE_MESSAGE_1 = "Please enter start date in YYYY-MM-DD format: "
-INPUT_DATE_MESSAGE_2 = "Please enter end date in YYYY-MM-DD format: "
-MONDAY_MESSAGE = "אין לי ויניגרט"
 MONDAY = 1
 
 
 def vinaigrette():
-    # main function for vinaigrette code
-    start_date = read_date(INPUT_DATE_MESSAGE_1)  # get first date
-    end_date = read_date(INPUT_DATE_MESSAGE_2)  # get second date
+    """
+     Main function for vinaigrette code
+    :return:
+    """
+    start_date = read_date("Please enter start date in YYYY-MM-DD format: ")  # get first date
+    end_date = read_date("Please enter end date in YYYY-MM-DD format: ")  # get second date
     rand_date = random_date(start_date, end_date, '%Y-%m-%d', random.random())  # generate random date
     print(rand_date)
     if check_if_monday(rand_date):  # check if date is monday and if yes print "אין לי ויניגרט"
-        print(MONDAY_MESSAGE)
+        print("אין לי ויניגרט")
 
 
 def read_date(string):
-    # function to get date from user. gets an str and returns date.
-    in_date = input(string)
-    return in_date
+    """
+     function to get date from user. gets an str and returns date.
+    :param string: date
+    :return: date
+    """
+    user_date_input = input(string)
+    return user_date_input
 
 
-def random_date(start, end, time_format, prop):
-    # function to generate random date. gets start- beginning of range
-    # end - end of range, time_format, prop- proportion of a range
-    # returns random date in range
-    stime = time.mktime(time.strptime(start, time_format))
-    etime = time.mktime(time.strptime(end, time_format))
-    ptime = stime + prop * (etime - stime)
+def random_date(start_date, end_date, time_format, prop):
+    """
+     function to generate random date.
+    :param start_date:beginning of range
+    :param end_date:end of range
+    :param time_format:YYYY-MM-DD
+    :param prop:proportion of a range
+    :return:random date in range
+    """
+    start_time = time.mktime(time.strptime(start_date, time_format))
+    end_time = time.mktime(time.strptime(end_date, time_format))
+    ptime = start_time + prop * (end_time - start_time)
 
     return datetime.date(time.localtime(ptime).tm_year,
                          time.localtime(ptime).tm_mon,
@@ -38,7 +47,11 @@ def random_date(start, end, time_format, prop):
 
 
 def check_if_monday(date):
-    # function gets random date and checks if it falls on a monday returns boolean
+    """
+    function gets random date and checks if it falls on a monday.
+    :param date: random date in range
+    :return: boolean if date falls on monday.
+    """
     return date.isoweekday() == MONDAY
 
 
